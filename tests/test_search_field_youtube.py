ceaa90d42bf7148d03ab492f9de1b0d7724efd51
artem <abashlak@waverleysoftware.com>
2020-10-02 15:59:04 +0300

    Create POM todo list

--- tests/test_search_field_youtube.py
+++ tests/test_search_field_youtube.py
@@ -1,20 +1,12 @@
 import clipboard
 import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-from core.locators.home import HomepageLocators
+from core.locators.homepage_locators import HomepageLocators
 from core.services.email_service import EmailService
 from core.utility.utilities import UtilityMethods
-
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")  # Runs Chrome in headless mode.
-options.add_argument('--no-sandbox')  # # Bypass OS security model
-options.add_argument('start-maximized')
-options.add_argument('disable-infobars')
-options.add_argument("--disable-extensions")
 
 locators = HomepageLocators()
 url = "https://www.youtube.com/"
@@ -23,15 +15,14 @@ url = "https://www.youtube.com/"
 class Test_Share_Button:
     @pytest.mark.skip("")
     def test_open_youtube(self, browser) -> None:
-        # driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
         browser.get(url)
         title = browser.title
         assert "YouTube" in title
 
     def test_search_field(self, browser) -> None:
         browser.get(url)
-        WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, locators.search_field))).send_keys(
-            "pytest")
+        WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, locators.search_field))) \
+            .send_keys("pytest")
         browser.find_element_by_css_selector(locators.search_button).click()
         elem = browser.find_elements_by_id(locators.video_title)[0]
         url_first_video_after_search = elem.get_attribute(locators.video_href)
@@ -41,8 +32,8 @@ class Test_Share_Button:
 
     def test_share_button_url(self, browser) -> None:
         browser.get(url)
-        WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, locators.search_field))).send_keys(
-            "pytest")
+        WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, locators.search_field))) \
+            .send_keys("pytest")
         browser.find_element_by_css_selector(locators.search_button).click()
         elem = browser.find_elements_by_id(locators.video_title)[0]
         url_first_video_after_search = elem.get_attribute(locators.video_href)
